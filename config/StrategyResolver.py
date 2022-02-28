48161025d35c1dc6114d251dc5aa222bc5758a6f
sajanrajdev <sajanrajdev10@gmail.com>
2022-02-27 21:10:36 -0700

    test: resolver initial implementations

--- config/StrategyResolver.py
+++ config/StrategyResolver.py
@@ -1,8 +1,8 @@
 from helpers.StrategyCoreResolver import StrategyCoreResolver
 from rich.console import Console
+from brownie import interface
 
 console = Console()
-
 
 class StrategyResolver(StrategyCoreResolver):
     def get_strategy_destinations(self):
@@ -24,45 +24,88 @@ class StrategyResolver(StrategyCoreResolver):
         Specifies extra check for ordinary operation on withdrawal
         Use this to verify that balances in the get_strategy_destinations are properly set
         """
-        assert False
+        assert True
 
     def hook_after_confirm_deposit(self, before, after, params):
         """
         Specifies extra check for ordinary operation on deposit
         Use this to verify that balances in the get_strategy_destinations are properly set
         """
-        assert False
+        assert True
 
     def hook_after_earn(self, before, after, params):
         """
         Specifies extra check for ordinary operation on earn
         Use this to verify that balances in the get_strategy_destinations are properly set
         """
-        assert False
+        assert True
+
+    def confirm_harvest_events(self, before, after, tx):
+        key = "TreeDistribution"
+        assert key in tx.events
+        assert len(tx.events[key]) >= 1
+        for event in tx.events[key]:
+            keys = [
+                "token",
+                "amount",
+                "blockNumber",
+                "timestamp",
+            ]
+            for key in keys:
+                assert key in event
+
+            console.print(
+                "[blue]== Solidex Strat harvest() TreeDistribution State ==[/blue]"
+            )
+            self.printState(event, keys)
+
+        key = "PerformanceFeeGovernance"
+        assert key in tx.events
+        assert len(tx.events[key]) >= 1
+        for event in tx.events[key]:
+            keys = [
+                "destination",
+                "token",
+                "amount",
+                "blockNumber",
+                "timestamp",
+            ]
+            for key in keys:
+                assert key in event
+
+            console.print(
+                "[blue]== Solidex Strat harvest() PerformanceFeeGovernance State ==[/blue]"
+            )
+            self.printState(event, keys)
+
+        key = "Harvest"
+        assert key in tx.events
+        assert len(tx.events[key]) == 1
+        event = tx.events[key][0]
+        keys = [
+            "harvested",
+        ]
+        for key in keys:
+            assert key in event
+
+        console.print("[blue]== Helper Strat harvest() State ==[/blue]")
+        self.printState(event, keys)
+
+        key = "PerformanceFeeStrategist"
+        assert key not in tx.events
+        # Strategist performance fee is set to 0
 
     def confirm_harvest(self, before, after, tx):
         """
         Verfies that the Harvest produced yield and fees
         """
         console.print("=== Compare Harvest ===")
-        self.manager.printCompare(before, after)
-        self.confirm_harvest_state(before, after, tx)
+        self.confirm_harvest_events(before, after, tx)
+        super().confirm_harvest(before, after, tx)
 
         valueGained = after.get("sett.pricePerFullShare") > before.get(
             "sett.pricePerFullShare"
         )
-
-        # # Strategist should earn if fee is enabled and value was generated
-        # if before.get("strategy.performanceFeeStrategist") > 0 and valueGained:
-        #     assert after.balances("want", "strategist") > before.balances(
-        #         "want", "strategist"
-        #     )
-
-        # # Strategist should earn if fee is enabled and value was generated
-        # if before.get("strategy.performanceFeeGovernance") > 0 and valueGained:
-        #     assert after.balances("want", "governanceRewards") > before.balances(
-        #         "want", "governanceRewards"
-        #     )
 
     def confirm_tend(self, before, after, tx):
         """
@@ -73,3 +116,42 @@ class StrategyResolver(StrategyCoreResolver):
         (Strategy Must Implement)
         """
         assert True
+
+    def add_entity_balances_for_tokens(self, calls, tokenKey, token, entities):
+        entities["badgerTree"] = self.manager.strategy.badgerTree()
+        entities["strategy"] = self.manager.strategy.address
+        entities["lpDepositor"] = self.manager.strategy.lpDepositor()
+        entities["baseV1Router01"] = self.manager.strategy.baseV1Router01()
+        entities["solidSolidSexLp"] = self.manager.strategy.solidSolidSexLp()
+        entities["sexWftmLp"] = self.manager.strategy.sexWftmLp()
+        if self.manager.strategy.name() == "StrategySolidexSexWftmHelper":
+            entities["solidHelperVault"] = self.manager.strategy.solidHelperVault()
+        else:
+            entities["sexHelperVault"] = self.manager.strategy.sexHelperVault()
+
+        super().add_entity_balances_for_tokens(calls, tokenKey, token, entities)
+        return calls
+
+    def add_balances_snap(self, calls, entities):
+        super().add_balances_snap(calls, entities)
+        strategy = self.manager.strategy
+
+        solid = interface.IERC20(strategy.solid())
+        sex = interface.IERC20(strategy.sex())
+        solidsex = interface.IERC20(strategy.sex())
+        wftm = interface.IERC20(strategy.sex())
+        solidSolidSexLp = interface.IERC20(strategy.sex())
+        sexWftmLp = interface.IERC20(strategy.sex())
+        if strategy.name() == "StrategySolidexSexWftmHelper":
+            solidHelper = interface.IERC20(strategy.solidHelperVault())
+            calls = self.add_entity_balances_for_tokens(calls, "solidHelper", solidHelper, entities)
+        else:
+            sexHelper = interface.IERC20(strategy.sexHelperVault())
+            calls = self.add_entity_balances_for_tokens(calls, "sexHelper", sexHelper, entities)
+
+        calls = self.add_entity_balances_for_tokens(calls, "solid", solid, entities)
+        calls = self.add_entity_balances_for_tokens(calls, "sex", sex, entities)
+        calls = self.add_entity_balances_for_tokens(calls, "solidsex", solidsex, entities)
+        calls = self.add_entity_balances_for_tokens(calls, "wftm", wftm, entities)
+        calls = self.add_entity_balances_for_tokens(calls, "solidSolidSexLp", solidSolidSexLp, entities)
+        calls = self.add_entity_balances_for_tokens(calls, "sexWftmLp", sexWftmLp, entities)

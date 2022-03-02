ac3b3dce7e7b902a3336a371024bcc0d5127f34a
Ayush Shukla <shuklaayush247@gmail.com>
2022-03-02 19:50:57 +0530

    fix(test): add missing function

--- config/StrategyResolver.py
+++ config/StrategyResolver.py
@@ -1,6 +1,8 @@
 from helpers.StrategyCoreResolver import StrategyCoreResolver
 from rich.console import Console
+from helpers.utils import val
 from brownie import interface
+from tabulate import tabulate
 
 console = Console()
 
@@ -39,6 +41,17 @@ class StrategyResolver(StrategyCoreResolver):
         Use this to verify that balances in the get_strategy_destinations are properly set
         """
         assert True
+
+    def printState(self, event, keys):
+        table = []
+        nonAmounts = ["token", "destination", "blockNumber", "timestamp"]
+        for key in keys:
+            if key in nonAmounts:
+                table.append([key, event[key]])
+            else:
+                table.append([key, val(event[key])])
+
+        print(tabulate(table, headers=["account", "value"]))
 
     def confirm_harvest_events(self, before, after, tx):
         key = "TreeDistribution"
